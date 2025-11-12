cc0bc1ed71fe688da93eb6bb54c4918b31077b47
yunyun <imnp516@gmail.com>
2025-11-12 22:07:21 +0700

    update list fruit on array

--- src/UpdARRAYinState.jsx
+++ src/UpdARRAYinState.jsx
@@ -1,7 +1,24 @@
-import React, {useState} from 'react'
+
+import {useState} from 'react';
 
 function UpdARRAYinState(){
-    return(<></>)
+
+    const[foods, setFoods] = useState(["Apple", "Orange", "Banana"])
+
+    function handleAddFood(){
+
+    }
+
+    function handleRemoveFood(){
+
+    }
+ 
+    return(<div>
+            <h2>List of Foods</h2>
+            <ul>
+                {foods.map((food, index) => <li key={index}>{food}</li>)}    
+            </ul>    
+        </div>)
 }
 
 export default UpdARRAYinState;
